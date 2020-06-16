f67cf6c41781ecc430f94fb6a982a50909963734
Ay-ami <60237407+Ay-ami@users.noreply.github.com>
2020-06-16 01:02:11 -0400

    initial upload of main.c and input.txt
    main.c does currently have an error that causes it to crash

---
+++ main.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+Based on Wirth’s definition for EBNF we have the following rule:
+[ ] means an optional item,
+{ } means repeat 0 or more times.
+Terminal symbols are enclosed in quote marks.
+A period is used to indicate the end of the definition of a syntactic class.
+*/
+
+char * RESERVED_WORDS[] = {"const", "var", "procedure", "call", "if", "then", "else", "while", "do", "read", "write", "odd"};
+char SPECIAL_SYMBOLS[] = {'+', '-', '*', '/', '(' , ')', '=', ',' , '.', '<', '>',  ';' , ':' };
+
+// token types
+typedef enum {
+nulsym = 1, identsym, numbersym, plussym, minussym,
+multsym,  slashsym, oddsym, eqsym, neqsym, lessym, leqsym,
+gtrsym, geqsym, lparentsym, rparentsym, commasym, semicolonsym,
+periodsym, becomessym, lbracesym, rbracesym, ifsym, thensym,
+whilesym, dosym, callsym, constsym, varsym, procsym, writesym,
+readsym , elsesym
+}token_type;
+
+// opens the file
+FILE *openFile(char fileName[], char mode[], FILE *fp)
+{
+    fp=fopen(fileName, mode);
+    if(fp==NULL)
+    {
+        printf("Could not open %s\n", fileName);
+        exit(0);
+    }
+    printf("file opened\n");
+    return fp;
+}
+
+int main()
+{
+    FILE* fp;
+    fp=openFile("input.txt", "r", fp);
+
+    printf("%s", RESERVED_WORDS[0]);
+    printf(" %d", nulsym);
+
+    char achar;
+    int count = 0;
+    while(!feof(fp)) // "while not at the end of the file"
+    {
+      fscanf(fp, "%c", achar); // "scan in 3 numbers per line"
+      printf("%c", achar);
+      count++;
+    }
+
+
+
+    return 0;
+}
