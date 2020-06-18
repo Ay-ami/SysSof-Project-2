555804796c2a10a78b8c00221ab01a13f949d98d
Ay-ami <60237407+Ay-ami@users.noreply.github.com>
2020-06-17 21:08:59 -0400

    main.c update
    can read in line by line and character by character

--- main.c
+++ main.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 /*
-Based on Wirthâ€™s definition for EBNF we have the following rule:
+Based on Wirth’s definition for EBNF we have the following rule:
 [ ] means an optional item,
 { } means repeat 0 or more times.
 Terminal symbols are enclosed in quote marks.
 A period is used to indicate the end of the definition of a syntactic class.
 */
+
+#define MAX_DIGITS 5
+#define MAX_ID_LENGTH 11
 
 char * RESERVED_WORDS[] = {"const", "var", "procedure", "call", "if", "then", "else", "while", "do", "read", "write", "odd"};
 char SPECIAL_SYMBOLS[] = {'+', '-', '*', '/', '(' , ')', '=', ',' , '.', '<', '>',  ';' , ':' };
@@ -37,21 +41,38 @@ FILE *openFile(char fileName[], char mode[], FILE *fp)
 
 int main()
 {
-    FILE* fp;
+    FILE *fp, *fpw;
     fp=openFile("input.txt", "r", fp);
+    fpw=openFile("output.txt", "w", fpw);
 
+    //test global arrays
     printf("%s", RESERVED_WORDS[0]);
     printf(" %d", nulsym);
 
-    char achar;
 
+    // scan input.txt -> output.txt
+    char aline[100]; //I'm assuming no line will be longer than 60 characters
     while(!feof(fp)) // "while not at the end of the file"
     {
-      fscanf(fp, "%c", achar);
-      printf("%c", achar);
+        fgets(&aline, 100, fp);
+        fprintf(fpw, "%s", aline);
+        //printf("\n%d", strlen(aline)-1);
+        //printf(" %d", strlen(test));
+    }
+    rewind(fp);
+    printf("\n");
+
+    //print character by character
+    char c;
+    while(!feof(fp))
+    {
+        fscanf(fp, "%c", &c);
+        printf("%c", c);
     }
 
 
+    fclose(fp);
+    fclose(fpw);
 
     return 0;
 }
